703ba7483caa8ba4e47a0ac0c0a747ec2ddc7e1d
Dave <dave@agileinf.com>
2017-06-11 23:20:09 +0530

    full up version

--- wp-content/plugins/ai-core/ai-core.php
+++ wp-content/plugins/ai-core/ai-core.php
@@ -13,12 +13,26 @@ Author URI: http://www.agileinf.com
 */
 class AiCore_Plugin {
 	private $templatePath = WP_PLUGIN_URL . "/ai-core/templates";
+	private $posts;
 	function __construct() {
-		add_shortcode("quote", array($this, 'quote'));
+		add_shortcode("posts_from_category_1", array($this, 'posts_from_category_1'));
 	}
 
-	public function quote() {
-	   include($this->templatePath . "/mytemplate.php");
+	public function posts_from_category_1( $atts ) {
+		ob_start();
+		global $posts;
+		$args = array('category_name' => $atts['category_slug'] );
+		$posts = get_posts( $args );			
+		$this->render($posts);
+	   	return ob_get_clean();
+	}
+
+	public function render($posts){
+		?>
+		<pre>
+			<?php //print_r($posts); ?>
+		</pre>
+		<?php
 	}
 }
 $obj = new AiCore_Plugin();
